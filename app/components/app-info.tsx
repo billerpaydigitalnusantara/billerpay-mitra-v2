d3e51ab35b8366c59bc3e0b0fb12a61432b185c3
Adzhar Amrullah <adzharamrullah@gmail.com>
2025-02-13 21:53:06 +0700

    feat: app info

--- app/components/app-info.tsx
+++ app/components/app-info.tsx
@@ -9,6 +9,9 @@ import {
   Button,
 } from "@heroui/react";
 import { XMarkIcon, ArrowPathIcon } from "@heroicons/react/24/outline";
+import { useEffect, useState } from "react";
+import api from "@/lib/axios";
+import { AppInfoResponse } from "@/types";
 
 interface AppInfoProps {
   isOpen: boolean;
@@ -16,6 +19,20 @@ interface AppInfoProps {
 }
 
 const AppInfo: React.FC<AppInfoProps> = ({ isOpen, onOpenChange }) =>  {
+  const [appInfo, setAppInfo] = useState<AppInfoResponse>({} as AppInfoResponse)
+
+  const fetchAppInfo = async () => {
+    const response = await api.get('/KONFIG/URL_REQUEST/MITRA')
+    setAppInfo(response.data)
+  }
+
+  const onHandleReload = () => {
+    fetchAppInfo()
+  }
+
+  useEffect(() => {
+    fetchAppInfo()
+  }, [isOpen])
 
   return (
     <Modal isOpen={isOpen} placement="top-center" onOpenChange={onOpenChange}>
@@ -27,32 +44,32 @@ const AppInfo: React.FC<AppInfoProps> = ({ isOpen, onOpenChange }) =>  {
               <div className="flex flex-col gap-4">
                 <div>
                   <h4 className="font-medium text-gray-600">Versi Aplikasi</h4>
-                  <p className="text-sm text-gray-600">1.0.0</p>
+                  <p className="text-sm text-gray-600">{ appInfo.versi }</p>
                 </div>
                 <div>
                   <h4 className="font-medium text-gray-600">Server Info</h4>
                   <div className="grid grid-cols-[1fr,1fr,3fr] gap-2">
                     <div className="text-sm text-gray-600 flex items-center">Transaksi</div>
                     <div> : </div>
-                    <div className="text-sm text-gray-600 font-medium">api-report</div>
+                    <div className="text-sm text-gray-600 font-medium">{ appInfo.transaksi }</div>
                   </div>
                   <div className="grid grid-cols-[1fr,1fr,3fr] gap-2">
                     <div className="text-sm text-gray-600 flex items-center">Kolektif</div>
                     <div> : </div>
-                    <div className="text-sm text-gray-600 font-medium">api-report</div>
+                    <div className="text-sm text-gray-600 font-medium">{ appInfo.kolektif }</div>
                   </div>
                   <div className="grid grid-cols-[1fr,1fr,3fr] gap-2">
                     <div className="text-sm text-gray-600 flex items-center">Laporan</div>
                     <div> : </div>
-                    <div className="text-sm text-gray-600 font-medium">api-report</div>
+                    <div className="text-sm text-gray-600 font-medium">{ appInfo.laporan }</div>
                   </div>
                   <div className="grid grid-cols-[1fr,1fr,3fr] gap-2">
                     <div className="text-sm text-gray-600 flex items-center">Excel</div>
                     <div> : </div>
-                    <div className="text-sm text-gray-600 font-medium">api-report</div>
+                    <div className="text-sm text-gray-600 font-medium">{ appInfo.excel }</div>
                   </div>
                 </div>
-                <Button color="primary" startContent={<ArrowPathIcon className="size-5"/>} >Reload Server</Button>
+                <Button color="primary" startContent={<ArrowPathIcon className="size-5"/>} onPress={onHandleReload}>Reload Server</Button>
               </div>
             </ModalBody>
             <ModalFooter>
